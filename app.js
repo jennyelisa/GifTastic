9236dc75f29788dc76a2c535602a78557118006f
Jenny <jennyelisa07@aol.com>
2019-05-22 19:21:24 -0400

    psuedocode, logic started

---
+++ app.js
@@ -0,0 +1,20 @@
+// create array of strings to be searched
+var topics = ["pizza", "cake", "nachos", "tacos", "ice cream", "popsicle", "chocolate"];
+va queryURL = 
+// app will take these topics and create a button to the html
+//try using a loop to append button for each string in the array activity 6, 7, 8, 9
+function topicName () {
+    var topicName = $(this).attr("data-name");
+    console.log(topicName);
+}
+//when user clicks on a button, the page should grab 10 static (non animated gif image)
+//fron gihpy api and they should appear on the page  activity 15
+
+//every image should have its rating displayed 
+
+// ((((EXTRA))))
+//add a form to the page that will take the value from the input box and it gets added 
+//to the topics array. then make a function call that takes each topic in the
+//arry and remakes the buttons on the page 
+//need to make an input type box to append to the html
+var newTopic = $("#user-input").val() //this is grab the text from the input box
